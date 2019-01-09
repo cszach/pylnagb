65310f1e0bd3e6aff7bbc1005897f8d05df70ed3
Nguyen Hoang Duong <you_create@protonmail.com>
2019-01-09 15:48:45 +0700

    raw/vectorutils.py: Fix add_cartes() and other functions

--- raw/vectorutils.py
+++ raw/vectorutils.py
@@ -1,8 +1,7 @@
 """
-vectorutils.py - Vector operations implemented for raw representation of vectors
-which are Python lists or tuples or any iterable that works with len() and
-numeric indexing. The operations work with 2D and 3D vectors only. Vectors in
-other dimensional spaces are not supported.
+vectorutils.py - Vector operations implemented for raw representations of
+vectors which are Python lists or tuples. The operations work with 2D and 3D
+vectors only. Vectors in other dimensional spaces are not supported.
 
 The same operations but are implemented to work with objects of class Vector
 are defined in the Vector class (see /assets/vector.py).
@@ -10,29 +9,32 @@ are defined in the Vector class (see /assets/vector.py).
 This is free and unencumbered software released into the public domain.
 For more information, please refer to <http://unlicense.org>
 """
-import sys
-sys.path.insert(0, "../sys")
+from sys import path as syspath
 from math import sqrt, degrees, cos, sin, acos, atan, radians
-from collections import Iterable
+syspath.insert(0, "../sys")
+syspath.insert(0, "../assets")
 from linagb_error import *
+from coordsys import *
 
 
 def validate_vector(obj, throwerr=False):
     """
-    Given an object obj, check if it is iterable or otherwise will work with
-    the operations implemented in this Python code file.
+    Given an object obj, check if it is a valid raw representation of a real
+    mathematical vector. An accepted object would:
+        1. be a Python list or Python tuple
+        2. be 2 or 3 items in length
 
     :param obj: Test subject
     :param throwerr: Raise an error if the check returns false.
     :return: True if obj is a valid raw representation of mathematical vectors,
     False otherwise
     """
-    if isinstance(obj, Iterable) and type(obj) is not str and 1 < len(obj) < 4:
+    if isinstance(obj, (list, tuple)) and 1 < len(obj) < 4:
         return True
     else:
         if throwerr:
             raise TypeError("A given object is not an accepted representation"
-                            " of a vector")
+                            " of a vector (must be a Python list or tuple)")
         return False
 
 
@@ -44,7 +46,7 @@ def two_to_three(vector_2):
     :param vector_2: Input vector.
     :return: Converted vector.
     """
-    if validate_vector(vector_2):
+    if validate_vector(vector_2, True):
         return vector_2 if len(vector_2) == 3 else vector_2 + [0]
 
 
@@ -117,42 +119,46 @@ def pol(vector, physics=False):
             return [r, azimuth, inclination]
 
 
-def add_cartes(*args, fixdim=False):
+def add_cartes(*args, fix_dim=False, base_dim=None):
     """
     Add multiple vectors. The coordinates are assumed to be Cartesian. If
     vectors of different dimensions are given, by default the function will
-    raise a DimensionError and return an empty Python list. This behavior can be
-    disabled by setting parameter fixdim to True (it is False by default).
+    raise a DimensionError and return an empty Python list. This behavior can
+    be disabled by setting parameter fix_dim to True (it is False by default).
     A coordinate is added in the case of converting a 2D vector to a 3D one,
     and is removed in the opposite case. All 3D vectors will be converted to 2D
     vectors if the first vector given in the parameters is 2D and vice versa.
 
     :param args: Vectors to add
-    :param fixdim: Fix mismatch of dimensions? (True if yes)
-    :return: Result of adding. If only one vector is given, return that same
-    vector. If any error occurs (mismatch dimensions, object not valid,...),
-    an error is raised and an empty list ( [] ) is returned.
+    :param fix_dim: Option to fix mismatching dimensions
+    :param base_dim: If there are multiple vectors given, convert all vectors
+    to base_dim coordinate system. The result vector will also be in this
+    coordinate system. Valid values are either Cartesian or Cartesian_3 (these
+    are classes). If base_dim is None, use the first vector given in args's
+    coordinate system as base_dim
+    :return: Result of adding. If any error occurs (mismatch dimensions, object
+    not valid,...), an empty list ( [] ) is returned.
     """
     try:
-        if len(args) == 1:
-            if validate_vector(args[0], True):
-                return args[0]
-        else:
-            if validate_vector(args[0], True):
-                base_dim = len(args[0])
+        if validate_vector(args[0], True) and base_dim is None:
+            base_dim = Cartesian if len(args[0]) == 2 else Cartesian_3
+        elif base_dim not in (Cartesian, Cartesian_3):
+            raise ValueError
 
-            result = [0, 0] if base_dim == 2 else [0, 0, 0]
+        result = [0, 0] if base_dim == Cartesian else [0, 0, 0]
 
-            for v in args:
-                if validate_vector(v, True):
-                    if len(v) != base_dim and not fixdim:
-                        raise DimensionError()
+        for v in args:
+            if validate_vector(v, True):
+                if ((len(v) == 2 and base_dim == Cartesian_3)
+                        or (len(v) == 3 and base_dim == Cartesian)) \
+                        and not fix_dim:
+                    raise DimensionError
 
-                    result[0] += v[0]
-                    result[1] += v[1]
-                    if base_dim == len(v) == 3:
-                        result[2] += v[2]
+                result[0] += v[0]
+                result[1] += v[1]
+                if base_dim == Cartesian_3 and len(v) == 3:
+                    result[2] += v[2]
 
         return result
-    except (TypeError, DimensionError):
+    except (TypeError, ValueError, DimensionError) as e:
         return []
